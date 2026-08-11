fac4e8e3122fa6253227a58e1cdda2b3198ec5f7
Son Nguyen <xelon1929@gmail.com>
2026-08-11 20:10:20 +0700

    feat(brand): update official QTM MediaTech logo to Cloudinary CDN URL across the entire application

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -179,7 +179,7 @@ export default function Header() {
             style={{ background: "none", border: "none", cursor: "pointer", padding: 0 }}
           >
             <Image
-              src="/logo.png"
+              src="https://res.cloudinary.com/s3qilvce/image/upload/v1786453565/logo.png"
               alt="QTM MediaTech Logo"
               width={180}
               height={40}
